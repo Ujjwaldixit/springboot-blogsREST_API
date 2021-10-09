2a52dcb0b4f5da8f2dde6428e21979fd6c31d91a
Ujwal Dixit <ujjwal.dixit.17.1@mountblue.tech>
2021-10-08 22:18:45 -0400

    feat(JWT Authentication): cretead method to generate JSON web token

--- src/main/java/com/blogs/controller/UserController.java
+++ src/main/java/com/blogs/controller/UserController.java
@@ -1,19 +1,36 @@
 package com.blogs.controller;
 
+import com.blogs.config.JwtTokenUtil;
+import com.blogs.model.AuthenticationRequest;
+import com.blogs.model.AuthenticationResponse;
 import com.blogs.model.User;
 import com.blogs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
@@ -32,8 +49,18 @@ public class UserController {
         return "redirect:/login";
     }
 
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "loginForm";
+    @PostMapping("/login")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect UserName or Password", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt=jwtTokenUtil.generateToken(userDetails);
+
+        return  ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
--- src/main/java/com/blogs/model/AuthenticationResponse.java
+++ src/main/java/com/blogs/model/AuthenticationResponse.java
@@ -4,11 +4,11 @@ public class AuthenticationResponse {
 
     private String jwt;
 
-    public String getJwt() {
-        return jwt;
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
     }
 
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
+    public String getJwt() {
+        return jwt;
     }
 }
